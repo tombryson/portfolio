e759901a025ddced39778d6caf2887554879fbba
tombryson <thomas.bryson@outlook.com>
2022-09-17 17:56:18 +1000

    changes to css

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import react from 'react'
+import { useEffect, Helmet } from 'react'
 import './App.css';
 import TopBackgroundLight from './images/top-background-light.gif'
 import TopBackgroundDark from './images/top-background-dark.gif'
@@ -14,16 +14,15 @@ function App() {
   return (
     <>
     <div>
-      {icon}
       <div className="waterfalls background-overlay">
         <div className='content-top'>
           <h1>Welcome</h1>
           <div className='darkMode'>
-          {icon}
           </div>
         </div>
       </div>
     </div>
+    <div><h1 className='your-name'>Tom Bryson</h1></div>
     </>
   );
 }
